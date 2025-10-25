b8789d0bee3b2321106dd9c6898ce0e1f8237221
Drake Morgan <drake@morgancomputers.net>
2025-10-24 22:10:33 -0500

    started to split while waiting for the 48 varible problem to solve, still not finished

---
+++ src/solver/parser.rs
@@ -0,0 +1,114 @@
+pub enum HumanOperator {
+    And,
+    Or,
+    Not,
+    Implies, // becomes not a or b
+    Variable(String),
+    OpeningParenthesis,
+    ClosingParenthesis,
+    OpeningBracket,
+    ClosingBracket,
+    OpeningCurly,
+    ClosingCurly,
+}
+
+impl HumanOperator {
+    pub fn precedence(&self) -> usize {
+        match self {
+            HumanOperator::Variable(_) => 0,
+            HumanOperator::Not => 1,
+            HumanOperator::And => 2,
+            HumanOperator::Or => 3,
+            HumanOperator::Implies => 4,
+            _ => usize::MAX,
+        }
+    }
+
+    pub fn is_right_associative(&self) -> bool {
+        matches!(self, HumanOperator::Implies | HumanOperator::Not)
+    }
+}
+
+pub struct Sentance {
+    pub data: Vec<HumanOperator>,
+}
+
+impl From<String> for Sentance {
+    fn from(value: String) -> Self {
+        let mut data = Vec::new();
+        let mut buffer = String::new();
+
+        for ch in value.chars() {
+            match ch {
+                '(' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::OpeningParenthesis);
+                }
+                ')' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::ClosingParenthesis);
+                }
+                '[' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::OpeningBracket);
+                }
+                ']' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::ClosingBracket);
+                }
+                '{' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::OpeningCurly);
+                }
+                '}' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::ClosingCurly);
+                }
+
+                '|' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::Or);
+                }
+                '&' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::And);
+                }
+                '!' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::Not);
+                }
+                '>' => {
+                    push_buffer(&mut buffer, &mut data);
+                    data.push(HumanOperator::Implies);
+                }
+
+                ' ' => {
+                    push_buffer(&mut buffer, &mut data);
+                }
+
+                _ => buffer.push(ch),
+            }
+        }
+
+        push_buffer(&mut buffer, &mut data);
+
+        Sentance { data }
+    }
+}
+
+fn push_buffer(buffer: &mut String, data: &mut Vec<HumanOperator>) {
+    if buffer.is_empty() {
+        return;
+    }
+
+    let token = buffer.trim().to_lowercase();
+    let op = match token.as_str() {
+        "and" => HumanOperator::And,
+        "or" => HumanOperator::Or,
+        "not" => HumanOperator::Not,
+        "implies" => HumanOperator::Implies,
+        _ => HumanOperator::Variable(token),
+    };
+    data.push(op);
+    buffer.clear();
+}
